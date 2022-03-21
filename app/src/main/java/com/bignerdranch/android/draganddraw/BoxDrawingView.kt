3af5d8b7fdd9236720cf9f6c35a657b6ac67ce32
daniel-iroka <danieliroka67@gmail.com>
2022-03-21 21:59:39 +0100

    Attempt for third challenge. Made our app more accessible for the user.

--- app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
@@ -15,14 +15,13 @@ import android.view.View
 
 private const val TAG = "BoxDrawingView"
 private const val BOX_STATE = "box"
-private const val VIEW_STATE = "view"
 
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null) :
         View(context, attrs) {
 
     private var currentBox: Box? = null
     private var boxen = arrayListOf<Box>()  // list of boxes to be drawn out on the screen
-//    private var box = boxen as ArrayList
+    private var customView = View(context)
 
     private val boxPaint = Paint().apply {
         color = 0x22ff0000
@@ -34,17 +33,20 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) :
 
     init {
         isSaveEnabled = true
+        customView.contentDescription = R.string.place_finger_content_description.toString()
     }
+
 
 
     override fun onSaveInstanceState(): Parcelable {
         val bundle = Bundle()
         bundle.putParcelableArrayList(BOX_STATE, boxen)
-        bundle.putParcelable(VIEW_STATE, super.onSaveInstanceState())
+        bundle.putParcelable("view state", super.onSaveInstanceState())
 
         super.onSaveInstanceState()
         return bundle
     }
+
 
     override fun onRestoreInstanceState(state: Parcelable) {
         super.onRestoreInstanceState(state)
@@ -52,7 +54,7 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) :
         var viewState = state
         if (viewState is Bundle) {
             boxen = viewState.getParcelableArrayList(BOX_STATE)!!
-            viewState = viewState.getParcelable(VIEW_STATE)!!
+            viewState = viewState.getParcelable("view state")!!
         }
         val test = boxen
         Log.i(TAG, "Bundle received: $boxen and $test and finally $viewState")
@@ -72,6 +74,8 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) :
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val current = PointF(event.x, event.y)
         var action = ""
+        var cursor = 500.0000f
+
 
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
@@ -80,17 +84,35 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) :
                 currentBox = Box(current).also {
                     boxen.add(it)
                 }
+                customView.contentDescription = R.string.place_finger_content_description.toString()
             }
             MotionEvent.ACTION_MOVE -> {
                 action = "ACTION_MOVE"
                 // update the currentBox.end as the user moves his/her finger around the screen
                 updateCurrentBox(current)
+
+                /** LEFT RIGHT IMPLEMENTATION **/
+                if (current.x > cursor) {
+                    customView.contentDescription = R.string.move_right_content_description.toString()
+                    cursor = current.x
+
+                    Log.i(TAG, "Movement is Right")
+
+                } else if (current.x < cursor){
+                    customView.contentDescription = R.string.move_left_content_description.toString()
+                    cursor = current.x
+                    Log.i(TAG, "Movement is Left")
+
+                }
+
+                /** UP DOWN IMPLEMENTATION **/
             }
             MotionEvent.ACTION_UP -> {
                 action = "ACTION_UP"
                 // tells the last report of the currentBox as the user's finger leaves the screen
                 updateCurrentBox(current)
                 currentBox = null
+                customView.contentDescription = R.string.leave_content_description.toString()
             }
             MotionEvent.ACTION_CANCEL -> {
                 action = "ACTION_CANCEL"
